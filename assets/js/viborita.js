7a3130440ef5004b74412149b40b9d6b291cb229
Gaby Trejo <gabytrejo303@gmail.com>
2017-04-08 17:33:54 -0500

    Primer alcance completado

--- assets/js/viborita.js
+++ assets/js/viborita.js
@@ -4,29 +4,55 @@ var limites = document.getElementById("limites");
 var viborita = document.getElementById("viborita");
 
 document.addEventListener("keydown", movimiento);
+
+// Posiciones Iniciales
 var posicionX = 200;
 var posicionY = 200;
+
 // Movimiento sólo con teclado
 function movimiento(event){
   var flecha = event.keyCode;
   switch (flecha) {
     case 37: // Mover izquierda
       posicionX -= 100;
-      viborita.style.marginLeft = posicionX + "px";
+      if (posicionX < 0) {
+        alert("Perdiste");
+        sinMovimiento();
+      } else {
+        viborita.style.marginLeft = posicionX + "px";
+      }
       break;
     case 38: // Mover arriba
       posicionY -= 100;
-      viborita.style.marginTop = posicionY + "px";
+      if (posicionY < 0) {
+        alert("Perdiste");
+        sinMovimiento();
+      } else {
+        viborita.style.marginTop = posicionY + "px";
+      }
       break;
     case 39: // Mover derecha
       posicionX += 100;
-      viborita.style.marginLeft = posicionX + "px";
+      if (posicionX > 400) {
+        alert("Perdiste");
+        sinMovimiento();
+      } else {
+        viborita.style.marginLeft = posicionX + "px";
+      }
       break;
     case 40: // Mover abajo
       posicionY += 100;
-      viborita.style.marginTop = posicionY + "px";
+      if (posicionY > 400) {
+        alert("Perdiste");
+        sinMovimiento();
+      } else {
+        viborita.style.marginTop = posicionY + "px";
+      }
       break;
     default:
-
   }
+}
+
+function sinMovimiento(){
+  document.removeEventListener("keydown", movimiento);
 }
