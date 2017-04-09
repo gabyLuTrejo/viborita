94643217c32ee8518c2f514aba2af7fb3abe9a9a
Gaby Trejo <gabytrejo303@gmail.com>
2017-04-09 18:54:43 -0500

    Movimiento Continuo

--- assets/js/viborita.js
+++ assets/js/viborita.js
@@ -4,17 +4,39 @@ var limites = document.getElementById("limites");
 var viborita = document.getElementById("viborita");
 
 document.addEventListener("keydown", movimiento);
+var movimientoContinuo = window.setInterval(viboritaMoviendose, 500);
 
 // Posiciones Iniciales
-var posicionX = 200;
-var posicionY = 200;
+var posicionX = 250;
+var posicionY = 250;
+var direccion = "alto";
 
 // Movimiento sólo con teclado
 function movimiento(event){
   var flecha = event.keyCode;
   switch (flecha) {
-    case 37: // Mover izquierda
-      posicionX -= 100;
+    case 37: // Mover izquierda      posicionX -= 10;
+      return direccion = "izquierda";
+      break;
+    case 38: // Mover arriba
+      return direccion = "arriba";
+      break;
+    case 39: // Mover derecha
+      return direccion = "derecha";
+      break;
+    case 40: // Mover abajo
+      return direccion = "abajo";
+      break;
+    default:
+  }
+}
+
+
+// Movimiento cada 500 ms
+function viboritaMoviendose(){
+  switch (direccion) {
+    case "izquierda":
+      posicionX -= 10;
       if (posicionX < 0) {
         alert("Perdiste");
         sinMovimiento();
@@ -22,27 +44,26 @@ function movimiento(event){
         viborita.style.marginLeft = posicionX + "px";
       }
       break;
-    case 38: // Mover arriba
-      posicionY -= 100;
-      if (posicionY < 0) {
+    case "arriba":
+      posicionY -= 10;if (posicionY < 0) {
         alert("Perdiste");
         sinMovimiento();
       } else {
         viborita.style.marginTop = posicionY + "px";
       }
       break;
-    case 39: // Mover derecha
-      posicionX += 100;
-      if (posicionX > 400) {
+    case "derecha":
+      posicionX += 10;
+      if (posicionX > 490) {
         alert("Perdiste");
         sinMovimiento();
       } else {
         viborita.style.marginLeft = posicionX + "px";
       }
       break;
-    case 40: // Mover abajo
-      posicionY += 100;
-      if (posicionY > 400) {
+    case "abajo":
+      posicionY += 10;
+      if (posicionY > 490) {
         alert("Perdiste");
         sinMovimiento();
       } else {
@@ -50,9 +71,12 @@ function movimiento(event){
       }
       break;
     default:
+
   }
 }
 
+// Desabilitando Movimiento
 function sinMovimiento(){
   document.removeEventListener("keydown", movimiento);
+  clearInterval(movimientoContinuo);
 }
